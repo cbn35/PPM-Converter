b328eefe65cee38e3622b6211f6a0fc4b7c2763c
Chance Nelson <cbn35@nau.edu>
2017-09-07 08:23:32 -0700

    Added stdlib to the includes list

--- src/PPM.h
+++ src/PPM.h
@@ -3,6 +3,7 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "PPM.c"
 
@@ -10,4 +11,4 @@ FILE * create_ppm_p3(char * name, int width, int height, int max);  // creates a
 FILE * create_ppm_p6(char * name, int width, int height, int max);  // creates and prepares a new p6 file
 void write_pixel(int r, int g, int b, FILE * file);                 // writes a pixel to a ppm file
 FILE * convert_p3_to_p6(char * name, FILE * p3)                     // convert from p3 to p6
-FILE * convert_p3_to_p3(char * name, FILE * p6)                     // convert from p6 to p3
+FILE * convert_p6_to_p3(char * name, FILE * p6)                     // convert from p6 to p3
