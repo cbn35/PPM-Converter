c8bbdfc0fb25536aab90dd12a26cb6bf6fe47032
Chance Nelson <cbn35@nau.edu>
2017-09-11 15:12:18 -0700

    Removed comversion functions and their prototypes. This is outside the scope of a simple library and will be implemented in main

--- src/PPM.h
+++ src/PPM.h
@@ -10,7 +10,5 @@
 FILE * create_ppm_p3(char * name, int width, int height, int max);  // creates and prepares a new p3 file
 FILE * create_ppm_p6(char * name, int width, int height, int max);  // creates and prepares a new p6 file
 void write_pixel(int r, int g, int b, FILE * file);                 // writes a pixel to a ppm file
-FILE * convert_p3_to_p6(char * name, FILE * p3);                    // convert from p3 to p6
-FILE * convert_p6_to_p3(char * name, FILE * p6);                    // convert from p6 to p3
 int * get_ppm_file_information(FILE * file);                        // Get the formatted metadata of a ppm file
 int * read_image(FILE * image);                                     // Get an array of RGB values based on an image
